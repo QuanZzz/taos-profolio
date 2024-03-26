8087f1e36856db95bffc5c91fcc15383ee17dd76
QuanZzz <zhouquaneric@gmail.com>
2024-03-25 21:32:35 -0400

    ins url updated

--- src/utils/urls.js
+++ src/utils/urls.js
@@ -1,4 +1,4 @@
 export const INSTAGRAM_LINK =
-  "https://www.instagram.com/taosarchitectural?igsh=MXRya3I2dDRlZmI1Zg%3D%3D&utm_source=qr";
+  "https://www.instagram.com/taosarchitecture?igsh=MXRya3I2dDRlZmI1Zg%3D%3D&utm_source=qr";
 export const EMAIL_LINK = "info@taosco.ca";
 export const COMPANY_WEBSITE = "www.taosco.ca";
